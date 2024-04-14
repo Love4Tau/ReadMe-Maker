a3f035dc26eb14988de17559abc087b6694e59de
Lovely Tautuaa <lovelytau650@gmail.com>
2024-04-14 14:38:51 -0700

    Added questions to array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,16 @@
-// TODO: Include packages needed for this application
+const { default: inquirer } = require("inquirer");
 
+// TODO: Include packages needed for this application
+const fs = require(`fs`);
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+// Added questions in array to prompt for user input for the generator
+    `What was your motivation?`,
+    `Why did you build this project?`,
+    `What problem does it solve?`,
+    `What did you learn?`,
+    `What makes your project stand out?`,
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
