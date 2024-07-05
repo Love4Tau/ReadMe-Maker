26805f2576e009e6478f7b8486a98c1c31576612
Lovely Tautuaa <lovelytau650@gmail.com>
2024-07-05 07:50:08 -0700

    Finished project! Created all my questions and setup functions to write to new readme file and initialize the app. Created functions in markdown to handle the user's input.

--- Develop/index.js
+++ Develop/index.js
@@ -1,22 +1,94 @@
-const { default: inquirer } = require("inquirer");
+
 
 // TODO: Include packages needed for this application
-const fs = require(`fs`);
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generatorMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
 // Added questions in array to prompt for user input for the generator
-    `What was your motivation?`,
-    `Why did you build this project?`,
-    `What problem does it solve?`,
-    `What did you learn?`,
-    `What makes your project stand out?`,
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a short description explaining the what, why, and how of your project.",
+    },
+
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?",
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use:",
+    },
+
+    {
+        type: "input",
+        name: "credits",
+        message: "List your collaborators, if any, with links to their GitHub profiles.",
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "Please select which license you'd like to use for this project:",
+        choices: ["MIT License", "Apache License 2.0", "GNU Generl Public License v3.0", "Mozilla Public Licens 2.0", "none"],
+    },
+
+    {
+        type: "input",
+        name: "tests",
+        message: "Go the extra mile and write tests for your application. Then provide examples on how to run them here.",
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is the link for your github profile?",
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address:",
+    }
+    
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Success")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = async () => {
+    //  Pormpt for user input
+try {
+    const userInput = await inquirer.prompt(questions);
+
+    console.log(userInput);
+
+    let readmeData = generatorMarkdown(userInput);
+
+    writeToFile("README.md", readmeData);
+} catch (error) {
+    console.error(error)
+}
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,90 @@
+const licenseBadgeLink = {
+  "MIT License": {
+    badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    link: "https://opensource.org/licenses/MIT"
+  },
+
+  "Apache License 2.0": {
+    badge: "[![License: Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)",
+    link: "https://www.apache.org/licenses/LICENSE-2.0"
+  },
+
+  "GNU Generl Public License v3.0": {
+    badge: "[![License: GNU GPL v3.0](https://img.shields.io/badge/License-GNU%20GPL%20v3.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.html)",
+    link: "https://www.gnu.org/licenses/gpl-3.0.html"
+  },
+
+  "Mozilla Public Licens 2.0": {
+    badge: "[![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL%202.0-blue.svg)](https://www.mozilla.org/en-US/MPL/2.0/)",
+    link: "https://www.mozilla.org/en-US/MPL/2.0/"
+  },
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return licenseBadgeLink[license]?.badge || "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return licenseBadgeLink[license]?.link || "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license) {
+    return "";
+  }
+  return `
+  This project is licensed under ${license}.
+
+  ${renderLicenseBadge(license)}
+  ${renderLicenseLink(license)}
+  `
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Contact Information](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you had any questions, please feel free to contact me at ${data.email}.
+  Here is my GitHub profile link ${data.github}
+
+  `;
 }
 
 module.exports = generateMarkdown;
